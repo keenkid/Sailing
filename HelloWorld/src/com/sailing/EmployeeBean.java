2e17a5fe2a0bea8e1c3027c27d266a8d21b3fd83
keenkid.mao <keenkid.mao@fisglobal.com>
2018-03-28 18:02:04 +0800

    add a member

--- HelloWorld/src/com/sailing/EmployeeBean.java
+++ HelloWorld/src/com/sailing/EmployeeBean.java
@@ -1,5 +1,13 @@
 package com.sailing;
 
 public class EmployeeBean {
+	private String employeeName;
 
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
 }
