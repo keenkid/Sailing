df7d01b456e3b161cf721de291be698f676862f3
e1079580 <e1079580@AP-CHN-LP150074.FNFIS.com>
2018-04-02 09:48:07 +0800

    nothing changed

--- HelloWorld/src/com/sailing/HelloWorld.java
+++ HelloWorld/src/com/sailing/HelloWorld.java
@@ -1,10 +1,21 @@
 package com.sailing;
 
+import java.sql.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
-
+		DbHelper helper = new DbHelper();
+		String selectSql = "SELECT * FROM HumanResources.Employee";
+		ResultSet resultSet = helper.QueryData(selectSql);
+		try {
+			while(resultSet.next()){
+				System.out.println(resultSet.getString("NationalIDNumber") + ":" + resultSet.getString("Title"));
+			}
+		helper.ReleaseMemory();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
