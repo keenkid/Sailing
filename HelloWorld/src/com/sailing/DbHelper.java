e83c2d68c6ef62c86e602960529b1ef4c06a84eb
e1079580 <e1079580@AP-CHN-LP150074.FNFIS.com>
2018-04-16 14:12:59 +0800

    add mysql knowledge

--- HelloWorld/src/com/sailing/DbHelper.java
+++ HelloWorld/src/com/sailing/DbHelper.java
@@ -2,34 +2,51 @@ package com.sailing;
 
 import java.sql.*;
 
-//import com.microsoft.sqlserver.jdbc.*;
-
 public class DbHelper {
-	public DbHelper(){
-		
-	}
+	public DbHelper(){}
 	
 	private Connection sqlConnection = null;
 	private Statement statement = null;
 	private ResultSet resultSet = null;
 	
-	private Connection CreateConnection() throws SQLException{
-		String connectionString = "jdbc:sqlserver://AP-CHN-LP150074\\SQLEXPRESS:1433;databaseName=AdventureWorks;user=sa;password=maoyufeng";
+	private Connection CreateConnection() throws Exception{
+		//String connectionString = "jdbc:sqlserver://AP-CHN-LP150074\\SQLEXPRESS:1433;databaseName=AdventureWorks;user=sa;password=maoyufeng";
+		String connectionString = "jdbc:mysql://localhost:3306/keenkid?user=keenkid&password=maoyufeng&useSSL=false";
+		
+		Class.forName("com.mysql.jdbc.Driver");
 		
 		return DriverManager.getConnection(connectionString);
 	}
 	
-	public ResultSet QueryData(String selectSql){
+	public ResultSet QueryData(String selectSql) throws Exception{
 		try{			
 			sqlConnection = CreateConnection();
 			if(null != sqlConnection){
 				statement = sqlConnection.createStatement();
 				resultSet = statement.executeQuery(selectSql);
 			}
-		}catch(SQLException e){
+		} catch (SQLException e){
 			e.printStackTrace();
 		}
 		return resultSet;
+	}
+	
+	public int UpdateData(String updateSql) throws Exception{
+		int effectedRows = 0;
+		
+		try{
+			sqlConnection = CreateConnection();
+			if(null != sqlConnection){
+				statement = sqlConnection.createStatement();
+				effectedRows = statement.executeUpdate(updateSql);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlConnection.close();
+		}
+		
+		return effectedRows;
 	}
 	
 	public void ReleaseMemory(){
--- HelloWorld/src/com/sailing/HelloWorld.java
+++ HelloWorld/src/com/sailing/HelloWorld.java
@@ -6,16 +6,50 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		DbHelper helper = new DbHelper();
-		String selectSql = "SELECT * FROM HumanResources.Employee";
-		ResultSet resultSet = helper.QueryData(selectSql);
-		try {
-			while(resultSet.next()){
-				System.out.println(resultSet.getString("NationalIDNumber") + ":" + resultSet.getString("Title"));
-			}
+		
+		testInsert(helper);
+		
+		testUpdate(helper);
+		
+		testQuery(helper);
+		
 		helper.ReleaseMemory();
-		} catch (SQLException e) {
+	}
+	
+	private static void testQuery(DbHelper helper){
+		String selectSql = "SELECT * FROM CountryCode";
+		
+		try {
+			ResultSet resultSet = helper.QueryData(selectSql);
+			while(resultSet.next()){
+				System.out.println(String.format("%s : %s",resultSet.getString("Code"), resultSet.getString("CountryName")));
+			}
+			
+			helper.ReleaseMemory();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	private static void testInsert(DbHelper helper){
+		String insertSql = "INSERT CountryCode(Code,CountryName) Values('US', 'United States')";
+		
+		try{
+			int effectedRows = helper.UpdateData(insertSql);
+			System.out.println(String.format("%d row(s) inserted.", effectedRows));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void testUpdate(DbHelper helper){
+		String insertSql = "UPDATE CountryCode SET CountryName = 'United States' WHERE Code = 'US'";
+		
+		try{
+			int effectedRows = helper.UpdateData(insertSql);
+			System.out.println(String.format("%d row(s) updated.", effectedRows));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
